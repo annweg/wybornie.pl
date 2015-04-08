c0a215fb71af83844ddad6d8ba9982173e09cc6e
Anna Węgrzynowska <anna.wegrzynowska@hotmail.com>
2015-04-08 22:28:54 +0200

    Initial commit - working MVC structure

---
+++ src/pl/wybornie/controllers/DefaultControllers.java
@@ -0,0 +1,15 @@
+package pl.wybornie.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DefaultControllers {
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	   public String printHello(ModelMap model) {
+	      model.addAttribute("message", "Hello Spring MVC Framework!");
+	      return "hello";
+	   }
+}
