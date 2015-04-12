bb7c1f199ac3b21006bf2ed9dc5879c3a6e28076
Anna Węgrzynowska <anna.wegrzynowska@hotmail.com>
2015-04-12 23:51:09 +0200

    Old files and configuration deleted

--- project_workspace/wybornie/WebContent/database/categoryGroup.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `categorygroup` (`id`, `name_pl`, `name_en`, `name_de`) VALUES
-(1, 'Rodzaj posiłku', 'Type of meal', ''),
-(2, 'Rodzaj składników', 'Type of ingredients', ''),
-(3, 'Dieta', 'Diet', ''),
-(4, 'Pora dnia', 'Time of day', ''),
-(5, 'Kuchnie świata', 'Cuisine', ''),
-(6, 'Pora roku', 'Season', '');
--- project_workspace/wybornie/WebContent/database/difficulty.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `difficulty` (`id`, `name_pl`, `name_en`, `name_de`) VALUES
-(1, 'łatwe', 'easy', ''),
-(2, 'średnie', 'medium', ''),
-(3, 'trudne', 'hard', '');
